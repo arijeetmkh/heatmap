a5ed21bdefef2864f5fdec6ba7f58150de7004ca
Arijeet Mukherjee <arijeet_in@yahoo.co.in>
2014-05-14 15:32:01 +0530

    Cross Browser Fix: Position capture firefox fix

--- heatmap/async_code.js
+++ heatmap/async_code.js
@@ -1,30 +1,35 @@
 <script>
     <!--START of tracking code-->
 
-function getXPath( element ) {
-    var val=element.value;
-    var xpath = '';
-    for ( ; element && element.nodeType == 1; element = element.parentNode )
-    {
-                //alert(element);
-        var id = $(element.parentNode).children(element.tagName).index(element) + 1;
-        id > 1 ? (id = '[' + id + ']') : (id = '');
-        xpath = '/' + element.tagName.toLowerCase() + id + xpath;
+    function getXPath( element ) {
+        var val=element.value;
+            var xpath = '';
+            //Check if element node or not and set element as its parent node
+            for ( ; element && element.nodeType == 1; element = element.parentNode )
+            {
+                //get index ot element
+                var id = $(element.parentNode).children(element.tagName).index(element) + 1;
+                //Set index as xpath string
+                id > 1 ? (id = '[' + id + ']') : (id = '');
+                xpath = '/' + element.tagName.toLowerCase() + id + xpath;
+            }
+            return xpath;
     }
-    return xpath;
-}
 
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    $('body').on('click', function(t) {
-        var width = t.target.clientWidth;
-        var height = t.target.clientHeight;
-        var XPATH = getXPath(t.target);
-        var offsetx = t.offsetX;
-        var offsety = t.offsetY;
-        var params = 'xpath=' + encodeURIComponent(XPATH) + '&xratio=' + (width/offsetx) + '&yratio=' + (height/offsety) + '&' + Math.random();
-        $('<img src="http://127.0.0.1:8000/record?' + params + '">');
+        $('body').on('click', function(t) {
+            console.log(t);
+            var width = t.target.clientWidth;
+            var height = t.target.clientHeight;
+            var XPATH = getXPath(t.target);
+            //Offsetxy not available on firefox
+            var offsetx = (t.offsetX || (t.pageX - $(t.target).offset().left));
+            var offsety = (t.offsetY || (t.pageY - $(t.target).offset().top));
+            var params = 'xpath=' + encodeURIComponent(XPATH) + '&xratio=' + (width/offsetx) + '&yratio=' + (height/offsety) + '&' + Math.random();
+            $('<img src="http://127.0.0.1:8000/record?' + params + '">');
+        });
+
     });
-});
     <!--END of tracking code-->
 </script>
